5ce5c75be3bf656b76c62a708507c807caf9d118
kstrodeveloper <aynerjosec@gmail.com>
2026-04-18 18:49:30 -0500

    refactor(utilidade): aplicar patron 1 - nombres con sentido.

--- src/main/java/com/mycompany/restauranteelbuensabor/service/Utilidades.java
+++ src/main/java/com/mycompany/restauranteelbuensabor/service/Utilidades.java
@@ -1,70 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.restauranteelbuensabor.service;
 
 import com.mycompany.restauranteelbuensabor.model.Datos;
 
 public class Utilidades {
-    public static double calcular(double pr, double cn, double dc, double iv, double pp, int ni, boolean ap) {
-        double res = 0;
-        double tmp = 0;
-        double aux2 = 0;
-        // calcula el resultado
-        res = pr * cn;
-        if (dc > 0) {
-            res = res - (res * dc);
+
+    public static double calcularConImpuestosYPropina(
+            double precioUnitario,
+            double cantidad,
+            double porcentajeDescuento,
+            double porcentajeIva,
+            double porcentajePropina,
+            int numeroProductos,
+            boolean aplicarPropina) {
+
+        double subtotal = precioUnitario * cantidad;
+
+        if (porcentajeDescuento > 0) {
+            subtotal = subtotal - (subtotal * porcentajeDescuento);
         }
-        tmp = res * iv;
-        res = res + tmp;
-        if (ap) {
-            res = res + (res * pp);
+
+        double iva = subtotal * porcentajeIva;
+        subtotal = subtotal + iva;
+
+        if (aplicarPropina) {
+            subtotal = subtotal + (subtotal * porcentajePropina);
         }
-        // imprime restaurante
+
         System.out.println("RESTAURANTE EL BUEN SABOR - calculo aplicado");
-        aux2 = res;
-        return aux2;
+        return subtotal;
     }
 
-    public static boolean hayProductos() {
-        int cont = 0;
-        int i = 0;
-        while (i < Datos.cantidades.length) {
-            if (Datos.cantidades[i] > 0) {
-                cont = cont + 1;
+    public static boolean hayProductosEnPedido() {
+        int cantidadProductos = 0;
+        int indice = 0;
+        while (indice < Datos.cantidades.length) {
+            if (Datos.cantidades[indice] > 0) {
+                cantidadProductos = cantidadProductos + 1;
             }
-            i++;
-        } // fin while
-        // reinicia si no hay nada - efecto secundario no documentado
-        if (cont == 0) {
+            indice++;
+        }
+        if (cantidadProductos == 0) {
             Datos.total = 0;
             Datos.temporal = "";
         }
-        return cont > 0;
+        return cantidadProductos > 0;
     }
 
     public static void reiniciar() {
-        // metodo antiguo de calculo - pendiente revisar
-        // public static double calcOld(double precio, int cantidades){
-        // double resultado = 0;
-        // resultado = precio * cantidades;
-        // resultado = resultado + (resultado * 0.19);
-        // if(resultado > 50000){
-        // resultado = resultado + (resultado * 0.10);}
-        // System.out.println("RESTAURANTE EL BUEN SABOR");
-        // System.out.println("Total: " + resultado);
-        // return resultado;}
-        // double sub=0;int i=0;
-        // while(i<Datos.nom.length){
-        // sub=sub+Datos.p[i]*Datos.cantidades[i];i++;}
-        // if(sub>50000){ sub=sub+(sub*0.19); sub=sub+(sub*0.10); }
-        // else{ sub=sub+(sub*0.19); }
-        // Datos.tot=sub;
-        int i = 0;
-        while (i < Datos.cantidades.length) {
-            Datos.cantidades[i] = 0;
-            i++;
+        int indice = 0;
+        while (indice < Datos.cantidades.length) {
+            Datos.cantidades[indice] = 0;
+            indice++;
         }
         Datos.total = 0;
         Datos.estado = 0;
