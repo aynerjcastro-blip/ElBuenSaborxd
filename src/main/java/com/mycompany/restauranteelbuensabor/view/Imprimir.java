b890ef7e950de131628043ba2bb39dec0675f77a
kstrodeveloper <aynerjosec@gmail.com>
2026-04-18 20:49:46 -0500

    refactor(Imprimir): crear submetodos imprmir el encabezado.

--- src/main/java/com/mycompany/restauranteelbuensabor/view/Imprimir.java
+++ src/main/java/com/mycompany/restauranteelbuensabor/view/Imprimir.java
@@ -7,13 +7,17 @@ package com.mycompany.restauranteelbuensabor.view;
 import com.mycompany.restauranteelbuensabor.model.Datos;
 
 public class Imprimir {
-    private static final String NOMBRE_RESTAURANTE  = "EL BUEN SABOR";
-    private static final String DIRECCION           = "Calle 15 #8-32, Valledupar";
-    private static final String NIT                 = "900.123.456-7";
-    private static final String SEPARADOR_DOBLE     = "========================================";
+    private static final String NOMBRE_RESTAURANTE = "EL BUEN SABOR";
+    private static final String DIRECCION = "Calle 15 #8-32, Valledupar";
+    private static final String NIT = "900.123.456-7";
+    private static final String SEPARADOR_DOBLE = "========================================";
 
+    public static void imprimirEncabezado() {
+        System.out.println(SEPARADOR_DOBLE);
+        System.out.println("    RESTAURANTE " + NOMBRE_RESTAURANTE);
+        System.out.println("    " + DIRECCION);
+        System.out.println("    NIT: " + NIT);
+        System.out.println(SEPARADOR_DOBLE);
+    }
 
- 
 }
-
-
