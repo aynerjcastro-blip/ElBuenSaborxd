e94da981827f152589a9835914662da7773ecf15
kstrodeveloper <aynerjosec@gmail.com>
2026-04-19 09:42:54 -0500

    feat: crear clase Producto para eliminar array paralelos.

---
+++ src/main/java/com/mycompany/restauranteelbuensabor/model/Producto.java
@@ -0,0 +1,18 @@
+package com.mycompany.restauranteelbuensabor.model;
+
+public class Producto {
+    private final String nombre;
+    private final double precio;
+
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+}
