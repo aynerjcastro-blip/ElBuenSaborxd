95c45e446d6e97353a94eb5eaeecbdc5170abb68
kstrodeveloper <aynerjosec@gmail.com>
2026-04-19 11:01:34 -0500

    feat(model): agregar clase ReglasNegocio para establecer las metricas economicas.

---
+++ src/main/java/com/mycompany/restauranteelbuensabor/model/ReglasNegocio.java
@@ -0,0 +1,9 @@
+package com.mycompany.restauranteelbuensabor.model;
+
+public class ReglasNegocio {
+    public static final double TASA_IVA = 0.19;
+    public static final double TASA_PROPINA = 0.10;
+    public static final double TASA_DESCUENTO = 0.05;
+    public static final double UMBRAL_PROPINA = 50000;
+    public static final int MIN_ITEMS_DESCUENTO = 3;
+}
