65af5ae7f35675ff7e6121307f669b439b388078
kstrodeveloper <aynerjosec@gmail.com>
2026-04-19 09:56:49 -0500

    feat(model): crear la clase Pedido para remplazar los arrays de cantidades

---
+++ src/main/java/com/mycompany/restauranteelbuensabor/model/Pedido.java
@@ -0,0 +1,10 @@
+package com.mycompany.restauranteelbuensabor.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pedido {
+    private final List<ItemPedido> items = new ArrayList<>();
+
+
+}
