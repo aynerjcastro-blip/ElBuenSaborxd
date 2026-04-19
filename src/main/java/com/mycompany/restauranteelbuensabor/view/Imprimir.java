5bfa978e7415fdb6cf5c193b59fa0f970e5b2edb
kstrodeveloper <aynerjosec@gmail.com>
2026-04-18 20:51:14 -0500

    refactor(Imprimir): agregar metodos para mostrar .

--- src/main/java/com/mycompany/restauranteelbuensabor/view/Imprimir.java
+++ src/main/java/com/mycompany/restauranteelbuensabor/view/Imprimir.java
@@ -11,6 +11,7 @@ public class Imprimir {
     private static final String DIRECCION = "Calle 15 #8-32, Valledupar";
     private static final String NIT = "900.123.456-7";
     private static final String SEPARADOR_DOBLE = "========================================";
+    private static final String SEPARADOR_SIMPLE = "----------------------------------------";
 
     public static void imprimirEncabezado() {
         System.out.println(SEPARADOR_DOBLE);
@@ -19,5 +20,36 @@ public class Imprimir {
         System.out.println("    NIT: " + NIT);
         System.out.println(SEPARADOR_DOBLE);
     }
+
+    public static void mostrarCarta() {
+        imprimirEncabezado();
+        System.out.println("    --- NUESTRA CARTA ---");
+        System.out.println(SEPARADOR_DOBLE);
+        int indice = 0;
+        while (indice < Datos.nombres.length) {
+            System.out.printf("%d. %-22s $%,.0f%n", (indice + 1), Datos.nombres[indice], Datos.precios[indice]);
+            indice++;
+        }
+        System.out.println(SEPARADOR_DOBLE);
+    }
+
+    public static void mostrarPedido() {
+        double subtotal = 0;
+        int indice = 0;
+        System.out.println("--- PEDIDO ACTUAL ---");
+        while (indice < Datos.nombres.length) {
+            if (Datos.cantidades[indice] > 0) {
+                System.out.printf("%-20s x%-6d $%,.0f%n",
+                    Datos.nombres[indice],
+                    Datos.cantidades[indice],
+                    Datos.precios[indice] * Datos.cantidades[indice]);
+                subtotal += Datos.precios[indice] * Datos.cantidades[indice];
+            }
+            indice++;
+        }
+        System.out.println(SEPARADOR_SIMPLE);
+        System.out.printf("%-27s $%,.0f%n", "Subtotal:", subtotal);
+    }
+
 
 }
